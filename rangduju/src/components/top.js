d104d4bec55ce9562df7602c955a211457ac57d6
Wuyou_lu <1414609941@qq.com>
2019-09-02 10:26:29 +0800

    2019.09.02
    修改bug

--- rangduju/src/components/top.js
+++ rangduju/src/components/top.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Input } from 'antd';
+// import { Input } from 'antd';
 
-const { Search } = Input;
+// const { Search } = Input;
 
 class Top extends React.Component {
     render() {
         return (
             <div className="top">
-                <div className="dis-flex">
+                <div className="dis-flex flex-w">
+                    <div className="l_top">
+                        <p className="title">让渡居 · 专业的短租解决方案</p>
+                        <p className="label1">我们可以为您的房子赚取高出传统长租最多<strong>40%</strong>的收益方案</p>
+                    </div>
                     <div className="left"><img src={require("../images/top.gif")} alt="动图"></img></div>
                     <div className="right">
-                        <p className="title">专业的短租解决方案</p>
-                        <p className="label1">我们可以为您的房子赚取高出传统长租最多<strong>40%</strong>的收益方案</p>
-                        <p className="label2">我们将提供从头到尾的一揽子服务</p>
+                        <p className="title">让渡居 · 专业的短租解决方案</p>
+                        <p className="label1">我们可以为您的房子<br />赚取高出传统长租最多<strong>40%</strong>的收益方案</p>
+                        {/* <p className="label2">我们将提供从头到尾的一揽子服务</p> */}
                         {/* <Search
                             placeholder="搜索"
                             enterButton="搜索"
