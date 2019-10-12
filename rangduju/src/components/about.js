c7d07646133fec61957dd9cef029480190a125cb
Wuyou_lu <1414609941@qq.com>
2019-10-12 17:32:39 +0800

    2019.10.12
    添加团队成员title

--- rangduju/src/components/about.js
+++ rangduju/src/components/about.js
@@ -8,7 +8,9 @@ class About extends React.Component{
                     <span className="title">让渡居是一家数字科技驱动的城市民宿管理服务机构</span>
                     <p className="synopsis">我们致力于提供专业的城市民宿管理方案，在营造安全、快捷、舒心、可靠的居住体验的同时，为客户创造长期领先于市场表现的经济回报。让渡居珍视每一位合作伙伴，并将共同努力，携手营造一流的新型城市民宿。<a href="https://mp.weixin.qq.com/s/n2C2IPwmw1zpEMuDDIUD3A" rel="noopener noreferrer" target="_blank">了解更多>></a></p>
                 </div>
-                <div className="distance"><iframe className="airtable-embed" src="https://airtable.com/embed/shrsHSNcv2C69FucP?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style={{background: 'transparent', border: '1px solid #ccc'}}></iframe></div>
+                <div className="distance">
+                    <div className="teamtitle">团队成员</div> 
+                    <iframe className="airtable-embed" src="https://airtable.com/embed/shrsHSNcv2C69FucP?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style={{background: 'transparent', border: '1px solid #ccc'}}></iframe></div>
                 <div className="exhibition dis-flex">
                     <div className="group1">
                         <div><img src={require("../images/tu1.jpg")} alt="图1"></img></div>
