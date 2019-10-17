8d3318ecabecaab1971995d2f76c7e7481631c44
Wuyou_lu <1414609941@qq.com>
2019-10-17 15:36:36 +0800

    2019.10.17
    首次提交build完成后的，进行测试。

---
+++ build/precache-manifest.a8d5387a60975cd1bbb2e7dbfa5ce196.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff29db3edc13c7fde7bbc5d4cc65bd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9d3b71995a716d27017",
+    "url": "/static/css/2.46749098.chunk.css"
+  },
+  {
+    "revision": "e5c173d9d9ffc3b415bd",
+    "url": "/static/css/main.2d8b30ac.chunk.css"
+  },
+  {
+    "revision": "d9d3b71995a716d27017",
+    "url": "/static/js/2.528c5abf.chunk.js"
+  },
+  {
+    "revision": "e5c173d9d9ffc3b415bd",
+    "url": "/static/js/main.d019b0de.chunk.js"
+  },
+  {
+    "revision": "2232cfef5adf396599c4",
+    "url": "/static/js/runtime~main.96c131fa.js"
+  },
+  {
+    "revision": "1fd95764b3e0dd6c0d0ce63faca149b6",
+    "url": "/static/media/2wm.1fd95764.jpg"
+  },
+  {
+    "revision": "8d00caa9071c8cbae78341650af2a3d4",
+    "url": "/static/media/chahua2.8d00caa9.png"
+  },
+  {
+    "revision": "9183a9a5f9e6ee0eae373050691eda48",
+    "url": "/static/media/chakanfangwulishishouyi.9183a9a5.png"
+  },
+  {
+    "revision": "a1975881cbfdbea7e2e0209b54dab805",
+    "url": "/static/media/foot.a1975881.gif"
+  },
+  {
+    "revision": "b14eb2ba1e24d948afa66cc8bdcc5984",
+    "url": "/static/media/hl.b14eb2ba.jpg"
+  },
+  {
+    "revision": "ed93568d77722b1b25cf9334787ec0d8",
+    "url": "/static/media/lishi.ed93568d.png"
+  },
+  {
+    "revision": "630cc342c240fdf1c51a0e165f553957",
+    "url": "/static/media/ruhehezuochangtu.630cc342.png"
+  },
+  {
+    "revision": "82618969f1ea99f746502b3bcde36cc8",
+    "url": "/static/media/top.82618969.gif"
+  },
+  {
+    "revision": "5e62edf36ba4c6466d693c2513e38167",
+    "url": "/static/media/tu1.5e62edf3.jpg"
+  },
+  {
+    "revision": "45b205fd8f20efdbb6d6f3b02a107056",
+    "url": "/static/media/tu10.45b205fd.jpg"
+  },
+  {
+    "revision": "78d0001eb3e0ebaad68d4c2824b083be",
+    "url": "/static/media/tu2.78d0001e.jpg"
+  },
+  {
+    "revision": "888130ce6509f2df68ab7ed463d4b028",
+    "url": "/static/media/tu3.888130ce.jpg"
+  },
+  {
+    "revision": "1658d524a5c97106202a291fea53d240",
+    "url": "/static/media/tu4.1658d524.jpg"
+  },
+  {
+    "revision": "0dfacb346ad8b3f141b3886dce421d89",
+    "url": "/static/media/tu5.0dfacb34.jpg"
+  },
+  {
+    "revision": "17b3ae8da8ac009a0c0a5f958f17065f",
+    "url": "/static/media/tu6.17b3ae8d.jpg"
+  },
+  {
+    "revision": "151e38ead8b61247f8114dc158f7a9ff",
+    "url": "/static/media/tu7.151e38ea.jpg"
+  },
+  {
+    "revision": "6f18c90c3e69376496c3ad189a0f541f",
+    "url": "/static/media/tu8.6f18c90c.jpg"
+  },
+  {
+    "revision": "13eb9ccd13d4c635902ec50fa31c8989",
+    "url": "/static/media/webfont.13eb9ccd.woff"
+  },
+  {
+    "revision": "2b175ac340b31a2fe666198f1085e317",
+    "url": "/static/media/webfont.2b175ac3.ttf"
+  },
+  {
+    "revision": "3efaee4c03a72bb47f42fd4a03221137",
+    "url": "/static/media/webfont.3efaee4c.svg"
+  },
+  {
+    "revision": "6e5a57e5b69d13f3d1a1f4d835244d5a",
+    "url": "/static/media/webfont.6e5a57e5.woff2"
+  },
+  {
+    "revision": "7245620415bc2ef1eb0da29a8728fdee",
+    "url": "/static/media/webfont.72456204.eot"
+  },
+  {
+    "revision": "bbbf6b7a44ec013d2c18b297490c6f8d",
+    "url": "/static/media/zhizhujuedinghezhuofangan.bbbf6b7a.png"
+  },
+  {
+    "revision": "e00c07f3356082163b73c739cbbfd964",
+    "url": "/static/media/zhuangshi.e00c07f3.png"
+  },
+  {
+    "revision": "2abf92a3837cfc62e40bcece567c2a11",
+    "url": "/static/media/组10.2abf92a3.jpg"
+  }
+]);
